36d0eae285e640253a774aa8a3e51a983dbeb0af
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2023-07-08 15:32:49 +0200

    MockUp backend classes

---
+++ app/src/Player.ts
@@ -0,0 +1,18 @@
+class Player extends User{
+
+    private currentPosition : number = 0
+    private isActive : boolean = false
+
+    private RollDice(){
+
+    }
+
+    public Move (position : number){
+
+    }
+
+    public toggleState(newState : boolean){
+        
+    }
+
+}

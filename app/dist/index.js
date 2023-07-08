8e8a5c6bc8a0f9d503d71c5af2c1276ed66ef716
FutureTechIo <morxtiger56@gmail.com>
2023-07-08 15:10:47 +0300

    adding packages
    adding typescript
    
    adding front-end folder structure
    
    adding git ignore
    
    Delete app/node_modules directory

---
+++ app/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getGreeting = void 0;
+function getGreeting(name) {
+    return `Hello, World !`;
+}
+exports.getGreeting = getGreeting;
+//# sourceMappingURL=index.js.map

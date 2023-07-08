d23d425c771e7a2f90abb34a2f12a20d070f0583
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2023-07-08 15:32:49 +0200

    MockUp backend classes

---
+++ app/src/Element.ts
@@ -0,0 +1,15 @@
+
+class element {
+
+    private type : string = "s"
+    private start : number = 0
+    private end : number = 0;
+
+    constructor (type : string , start : number , end : number){
+        this.type = type
+        this.start = start
+        this.end = end
+    }
+
+
+}
---
+++ app/src/User.ts
@@ -0,0 +1,20 @@
+
+class User{
+    private id : number = 0;
+    private username : string = "";
+    private isLockedIn : boolean = false;
+
+    public login(userName : string , password : string){
+
+    }
+
+
+    public register (userName: string , password: string){
+
+    }
+
+    public signOut(){
+
+    }
+
+}

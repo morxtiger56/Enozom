7e5c5fc4b156a58be7a5e6fffc4f10d5c8eb9e37
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2023-07-09 10:59:11 +0200

    server Layer

---
+++ app/dist/Logic/User.js
@@ -0,0 +1,15 @@
+"use strict";
+class User {
+    constructor() {
+        this.id = 0;
+        this.username = "";
+        this.isLoggedIn = false;
+    }
+    login(userName, password) {
+    }
+    register(userName, password) {
+    }
+    signOut() {
+    }
+}
+//# sourceMappingURL=User.js.map

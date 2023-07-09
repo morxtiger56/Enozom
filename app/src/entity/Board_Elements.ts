18ad073a10208f5c965ed8033e2e1aced8f41ba7
m13salama <mohamedsalama236@gmail.com>
2023-07-09 11:26:19 +0300

    entities last version isa

--- app/src/entity/Board_Elements.ts
+++ app/src/entity/Board_Elements.ts
@@ -1,8 +1,19 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, PrimaryColumn } from "typeorm"
 import { User } from "./User";
 import { Board } from "./Board";
 
 @Entity()
 export class Board_Elements {
-    
+    @PrimaryColumn("int")
+    @ManyToOne(() => Board, board_id => board_id.id)
+    board_id: number
+
+    @Column("varchar", { length: 1 })
+    element: string
+
+    @PrimaryColumn("int")
+    start: number
+
+    @Column("int")
+    end: number
 }

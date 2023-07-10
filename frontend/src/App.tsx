714946a56f590259e6834f608e9b0d1423f918f0
FutureTechIo <morxtiger56@gmail.com>
2023-07-10 14:24:58 +0300

    adding router in app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,35 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "./index.css";
+import Auth from "./pages/Auth";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+const router = createBrowserRouter([
+  { path: "/", element: <h1>Hello</h1> },
+  {
+    path: "/auth",
+    element: <Auth />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,36 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import "./index.css";
 import App from "./App";
-import Auth from "./pages/Auth";
-import Login from "./components/Login";
-import Register from "./components/Register";
 
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  {
-    path: "/auth",
-    element: <Auth />,
-    children: [
-      {
-        path: "login",
-        element: <Login />,
-      },
-      {
-        path: "register",
-        element: <Register />,
-      },
-    ],
-  },
-]);
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App></App>
   </React.StrictMode>
 );

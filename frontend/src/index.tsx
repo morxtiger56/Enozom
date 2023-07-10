9277b21482ec8394ea94c33a5faad28c031581d6
FutureTechIo <morxtiger56@gmail.com>
2023-07-11 00:59:41 +0300

    adding router transations

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,36 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import App from "./App";
-import Auth from "./pages/Auth";
-import Login from "./components/Login";
-import Register from "./components/Register";
-
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  {
-    path: "/auth",
-    element: <Auth />,
-    children: [
-      {
-        path: "login",
-        element: <Login />,
-      },
-      {
-        path: "register",
-        element: <Register />,
-      },
-    ],
-  },
-]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );

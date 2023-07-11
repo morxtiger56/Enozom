1b6d4789bea2764daf2087d0f6350ff0bec84df7
Ahmed Abdelaziz <Ahmed.Abdelaziz.GM@gmail.com>
2023-07-11 11:10:08 +0300

    Converted path resolve back to imports

--- app/src/data-source.ts
+++ app/src/data-source.ts
@@ -1,6 +1,11 @@
-import path from "path";
-import { DataSource } from "typeorm";
 import config from "./config/config";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+import { Game } from "./entity/Game";
+import { Board } from "./entity/Board";
+import { User_Game } from "./entity/User_Game";
+import { Board_Elements } from "./entity/Board_Elements";
+
 
 
 export const AppDataSource = new DataSource({
@@ -12,6 +17,6 @@ export const AppDataSource = new DataSource({
     database: config.MySQL_DB_TEST,
     synchronize: true,
     logging: false,
-    entities: [path.resolve("./entity/**/*.js")],
+    entities: [Game, Board_Elements, Board, User_Game, User],
     migrations: ["./migration/**/*.js"],
 })

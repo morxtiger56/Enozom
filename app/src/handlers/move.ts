72d42d7df88e1aee430e50db515420dbeeb28f16
FutureTechIo <morxtiger56@gmail.com>
2023-07-13 12:27:12 +0300

    Update move.ts

--- app/src/handlers/move.ts
+++ app/src/handlers/move.ts
@@ -1,11 +1,8 @@
-import { Request, Response, Router } from "express";
 import { GameDB } from "../DAO/GameDB";
 import Player from "../Logic/Player";
 
-
-export class MoveHandler{
-
-    public async  autoPlay(starttime: Date, gameID: number) {
+export class MoveHandler {
+    public async autoPlay(starttime: Date, gameID: number) {
         let currentGame;
 
         try {
@@ -17,17 +14,18 @@ export class MoveHandler{
         const lastMove: Date = currentGame.last_move;
 
         if (starttime.getTime() > lastMove.getTime()) {
-            const result = await Player.moveMyPlayer(currentGame.turn, currentGame);
-            const game = this.splitMyOutput(result)
-            return game
-        }else{
-            return null
+            const result = await Player.moveMyPlayer(
+                currentGame.turn,
+                currentGame
+            );
+            const game = this.splitMyOutput(result);
+            return game;
+        } else {
+            return null;
         }
     }
 
-
-    public splitMyOutput(result : number []){
-
+    public splitMyOutput(result: number[]) {
         let roll: number = 0;
         let steps: number[] = [];
         let nextPlayer: number = 0;
@@ -49,9 +47,9 @@ export class MoveHandler{
             turn: nextPlayer,
             state,
         };
-        return game
+        return game;
     }
-    public async move (userid , gameid,socket)  {
+    public async move(userid, gameid, socket) {
         try {
             let currentGame;
             try {
@@ -61,73 +59,59 @@ export class MoveHandler{
             }
 
             if (currentGame.turn == userid && currentGame.state == "start") {
-                const result = await Player.moveMyPlayer(userid, currentGame)
-                const outPut = this.splitMyOutput(result)
+                const result = await Player.moveMyPlayer(userid, currentGame);
+                const outPut = this.splitMyOutput(result);
 
-               this.broadcast(gameid,outPut,socket)
-
-
+                this.broadcast(gameid, outPut, socket);
 
                 let getMyGame;
 
-                const intervalId = setInterval(
-                    async () => {
-                        try {
-                            getMyGame = await GameDB.getGameById(gameid);
-                        } catch (e) {
-                            console.log(e);
-                        }
-                        const myState: string = getMyGame.state;
+                const intervalId = setInterval(async () => {
+                    try {
+                        getMyGame = await GameDB.getGameById(gameid);
+                    } catch (e) {
+                        console.log(e);
+                    }
+                    const myState: string = getMyGame.state;
 
-                        if (myState == "end") {
-                            clearInterval(intervalId);
-                            socket.to(gameid).emit("state", "end")
-                        }
-                        let autoPlayOutput =  this.autoPlay(new Date(), gameid);
-                        this.broadcast(gameid,autoPlayOutput,socket)
-                    },
-                    20000
-                );
-
+                    if (myState == "end") {
+                        clearInterval(intervalId);
+                        socket.to(gameid).emit("state", "end");
+                    }
+                    const autoPlayOutput = this.autoPlay(new Date(), gameid);
+                    this.broadcast(gameid, autoPlayOutput, socket);
+                }, 20000);
             } else {
                 return null;
             }
         } catch (err) {
             return null;
         }
-    };
-
-    public broadcast(gameid,outPut,socket){
-        socket.to(gameid).emit("rull_number", outPut.turn)
-        let start= outPut.steps[0]
-        let end1= outPut.steps[1]
-        let end2= -1
-        if(outPut.steps.length == 3)
-            end2= outPut.steps[2]
-        for(let i= start; i<=end1; i++){
-            socket.to(gameid).emit("move", i)
-        }
-        if(end2!= -1){
-            if(end1 > end2){
-                let t= end1
-                end1=end2
-                end2=t
-            }
-            for(let i= end1; i<=end2; i++){
-                socket.to(gameid).emit("move", i)
-            }
-        }
-        socket.to(gameid).emit("new_turn", outPut.turn)
-        socket.to(gameid).emit("state", outPut.state)
-
-
-
-
     }
 
+    public broadcast(gameid, outPut, socket) {
+        socket.to(gameid).emit("rull_number", outPut.turn);
+        const start = outPut.steps[0];
+        let end1 = outPut.steps[1];
+        let end2 = -1;
+        if (outPut.steps.length == 3) end2 = outPut.steps[2];
+        for (let i = start; i <= end1; i++) {
+            socket.to(gameid).emit("move", i);
+        }
+        if (end2 != -1) {
+            if (end1 > end2) {
+                const t = end1;
+                end1 = end2;
+                end2 = t;
+            }
+            for (let i = end1; i <= end2; i++) {
+                socket.to(gameid).emit("move", i);
+            }
+        }
+        socket.to(gameid).emit("new_turn", outPut.turn);
+        socket.to(gameid).emit("state", outPut.state);
+    }
 }
-
-
 
 // export default function moveRoutes(router: Router) {
 //     router.post("/move", move);

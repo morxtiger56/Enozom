c756ffc03195d04191081b90038c5d650ce264c5
Peteryasser <peteryasser80@gmail.com>
2023-07-13 13:04:24 +0300

    Join game in socket

--- app/migrations/sqls/20230709101518-Board-Elements-Table-down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE Board_Elements;
--- app/src/migration/1688866917083-mig1.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class Mig11688866917083 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> {}
-
-    public async down(queryRunner: QueryRunner): Promise<void> {}
-}
